88e8723b3115127eaa6f8cf38fe13bb653086b53
renato <renato.athaydes@curity.io>
2019-09-05 18:40:11 +0200

    Initial commit.

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,8 @@ import * as core from '@actions/core';
 
 async function run() {
   try {
-    const myInput = core.getInput('myInput');
-    core.debug(`Hello ${myInput}`);
+    const dartVersion = core.getInput('dart-version');
+    core.debug(`Hello ${dartVersion}`);
   } catch (error) {
     core.setFailed(error.message);
   }
