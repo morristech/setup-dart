34fe63dcbb3528d28b7d6a1285591960c08501de
Xavier Hainaux <xavier.hainaux@gmail.com>
2020-01-18 16:13:45 +0100

    Increment package version & rebuild

--- README.md
+++ README.md
@@ -1,3 +1,56 @@
 # Dart Setup GitHub Action
 
 GitHub Action that sets up the Dart SDK in a CI VM.
+
+## Usage
+
+#### Specific Dart version
+```yaml
+jobs:
+  test:
+    name: Run tests
+    runs-on: macos-latest
+    steps:
+      - uses: actions/checkout@v2
+      - uses: xvrh/setup-dart@v1
+        with:
+          dart-version: '2.7.0'
+```
+
+#### Latest version of a specific channel
+```yaml
+jobs:
+  test:
+    name: Run tests
+    runs-on: macos-latest
+    steps:
+      - uses: actions/checkout@v2
+      - uses: xvrh/setup-dart@v1
+        with:
+          dart-version: 'latest'
+          dart-channel: 'dev'
+```
+
+#### Multiple Dart versions
+```yaml
+jobs:
+  test:
+    name: Run tests
+    runs-on: macos-latest
+    steps:
+      - uses: actions/checkout@v2
+      - uses: xvrh/setup-dart@v1
+        with:
+          dart-version: 'latest'
+          dart-channel: 'stable'
+      - run: pub get && pub run test
+      - uses: xvrh/setup-dart@v1
+        with:
+          dart-version: 'latest'
+          dart-channel: 'dev'
+      - run: pub get && pub run test
+      - uses: xvrh/setup-dart@v1
+        with:
+          dart-version: '2.6.0'
+      - run: pub get && pub run test
+```
--- src/main.ts
+++ src/main.ts
@@ -5,36 +5,62 @@ import * as path from 'path';
 const IS_WINDOWS = process.platform === 'win32';
 const IS_MAC = process.platform === 'darwin';
 
-function getDartUrl(version: string, arch: string, channel: string): string {
+function getDartUrl(options: InstallOptions): string {
   let platform = IS_WINDOWS ? 'windows' : IS_MAC ? 'macos' : 'linux';
-  return `https://storage.googleapis.com/dart-archive/channels/${channel}/release/${version}/sdk/dartsdk-${platform}-${arch}-release.zip`;
+  return `https://storage.googleapis.com/dart-archive/channels/${options.channel}/release/${options.version}/sdk/dartsdk-${platform}-${options.architecture}-release.zip`;
 }
 
-async function installDart(version: string, arch: string, channel: string): Promise<void> {
-  let toolPath = tc.find('Dart', version);
+async function installDart(options: InstallOptions): Promise<void> {
+  let toolPath = tc.find('Dart', options.cacheKey);
 
   if (toolPath) {
     core.info(`Tool found in cache ${toolPath}`);
   } else {
-    const dartZip = await tc.downloadTool(getDartUrl(version, arch, channel));
+    const dartZip = await tc.downloadTool(getDartUrl(options));
     const unzippedDir = await tc.extractZip(dartZip);
     core.info(`dart-sdk extracted to ${unzippedDir}`);
     const dartSdkDir = path.join(unzippedDir, 'dart-sdk');
-    toolPath = await tc.cacheDir(dartSdkDir, 'Dart', version, arch);
+    toolPath = await tc.cacheDir(dartSdkDir, 'Dart', options.cacheKey);
   }
 
   let binDir = path.join(toolPath, "bin");
   core.addPath(binDir);
+  core.exportVariable('DART_SDK', toolPath);
 }
 
 async function run() {
   try {
-    const version = core.getInput('dart-version');
-    const arch = core.getInput('architecture');
-    const channel = core.getInput('dart-channel');
-    installDart(version, arch, channel);
+    await installDart(new InstallOptions(
+        core.getInput('dart-version'),
+        core.getInput('dart-channel'),
+        core.getInput('architecture'),
+    ));
   } catch (error) {
     core.setFailed(error.message);
+  }
+}
+
+class InstallOptions {
+  constructor(public readonly version: string,
+              public readonly channel: string,
+              public readonly architecture: string) {
+    if (!version) {
+      this.version = 'latest';
+    }
+    if (!channel) {
+      if (version.includes('-dev')) {
+        this.channel = 'dev';
+      } else {
+        this.channel = 'stable';
+      }
+    }
+    if (!architecture) {
+      this.architecture = 'x64';
+    }
+  }
+
+  get cacheKey(): string {
+    return `${this.version}-${this.channel}-${this.architecture}`;
   }
 }
 
